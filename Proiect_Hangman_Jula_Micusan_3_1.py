0cf4c89409ea804ca1000f8ec78e658cba5c0f11
roxanamicusan <84452829+roxanamicusan@users.noreply.github.com>
2021-05-19 22:23:55 +0300

    Finalizare fereastra 2 + incepere fereastra 3

--- Proiect_Hangman_Jula_Micusan_3_1.py
+++ Proiect_Hangman_Jula_Micusan_3_1.py
@@ -1,6 +1,7 @@
 from Lista_cuvinte import *
 import random
 from tkinter import *
+global x
 def fereastra1():
     fereastra=Tk()
     fereastra.title("Hangman Game")
@@ -22,18 +23,49 @@ def fereastra1():
         fereastra2.geometry("700x500")
         titlu2 = Label(fereastra2, text="Alegeti categoria dorita:\n1.Tari\n2.Orase\n3.Animale\n4.Random", font="Jokerman 35", bg="lightblue")
         titlu2.pack(anchor=N)
-        casuta=StringVar()
-        casuta_var=Entry(fereastra2,width=15,textvariable=casuta)
-        casuta_var.place(x=290,y=375)
+        def eval(event):
+            global x
+            x=str(casuta_var.get())
+            b=Button(fereastra2, text="Incepe Jocul!", font="Jokerman 20", width=12, height=1, command=fereastra_3)
+            b.place(x=230, y=400)
+            b.configure(bg="lightblue", anchor=CENTER, activatebackground="gray")
+        casuta_var = Entry(fereastra2, width=15)
+        casuta_var.place(x=290, y=375)
+        casuta_var.bind("<Return>", eval)
         def fereastra_3():
             fereastra2.destroy()
             fereastra3=Tk()
             fereastra3.title("Hangman Game")
             fereastra3.configure(bg="lightblue")
             fereastra3.geometry("700x500")
-            b=Button(fereastra2, text="Start",font="Jokerman 25", width=6, height=1, command=fereastra_3)
-            b.place(x=270, y=400)
-            b.configure(bg="lightblue",anchor=CENTER, activebackground="gray")
+            global x
+            if x=="1":
+                lb=Label(text="Ati ales categoria Tari:",font="Jokerman 15",bg="lightblue")
+                lb.pack(anchor=NW)
+                cuv=random.choice(Tari)
+                cuv=cuv.upper()
+            elif x=="2":
+                lb=Label(text="Ati ales categoria Orase:",font="Jokerman 15",bg="lightblue")
+                lb.pack(anchor=NW)
+                cuv=random.choice(Orase)
+                cuv=cuv.upper()
+            elif x=="3":
+                lb=Label(text="Ati ales categoria Animale:",font="Jokerman 15",bg="lightblue")
+                lb.pack(anchor=NW)
+                cuv=random.choice(Animale)
+                cuv=cuv.upper()
+            elif x=="4":
+                lb=Label(text="Ati ales categoria Random:",font="Jokerman 15",bg="lightblue")
+                lb.pack(anchor=NW)
+                cuv=random.choice(Random)
+                cuv=cuv.upper()
+            else:
+                lb=Label(text="Vi se va atribui un cuvant Random:",font="Jokerman 15",bg="lightblue")
+                lb.pack(anchor=NW)
+                cuv=random.choice(Random)
+                cuv=cuv.upper()
+            cuvant=Label(text="_ "*len(cuv),font="Jokerman 25",bg="lightblue",width=15)
+            cuvant.place(x=350,y=200)
     b=Button(fereastra,text="Start",font="Jokerman 25",width=6,height=1,command=fereastra_2)
     b.place(x=270,y=350)
     b.configure(bg="lightblue",anchor=CENTER,activebackground="gray")
