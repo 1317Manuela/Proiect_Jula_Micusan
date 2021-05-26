82f6976b198a356610d87ecb1d83e57e93a1964c
1317Manuela <manuela.jula@student.upt.ro>
2021-05-26 21:41:44 +0300

    fereastra0+retusari

--- Proiect_Hangman_Jula_Micusan_3_1.py
+++ Proiect_Hangman_Jula_Micusan_3_1.py
@@ -4,28 +4,18 @@ global fereastra
 global fereastra2
 global casuta_var
 global x
+global fereastra4
 global litera
 global ok
 global fereastra3
+global fereastra0
 global litere_folosite
 global cuv
+global cuvinte_folosite
 global cu
 global cuvant_nou
+global ct
 from tkinter import *
-def fereastra_4():
-    global ok
-    global fereastra3
-    fereastra3.destroy()
-    fereastra4 = Tk()
-    fereastra4.title("Hangman Game")
-    fereastra4.configure(bg="lightblue")
-    fereastra4.geometry("700x500")
-    if ok==1:
-        lb4=Label(fereastra4,text="Ai castigat!",font="Jokerman 50",bg="lightblue")
-        lb4.pack(anchor=N)
-    if ok==0:
-        lb5=Label(fereastra4,text="Ai pierdut!",font="Jokerman 50",bg="lightblue")
-        lb5.pack(anchor=N)
 def display_img(x):
     global fereastra3
     img1 = PhotoImage(file=x)
@@ -35,20 +25,28 @@ def display_img(x):
     imagine1.configure(bg="lightblue")
 def gaseste(event):
     global litera
-    global fereastra3
+    global fereastra
     global valoare
     global litere_folosite
+    global fereastra3
+    global fereastra
     global cuv
     global cu
     global ok
     global cuvant_nou
     global nr_imagini
-    valoare = str(litera.get())
+    global ct
+    global fereastra4
+    valoare =str(litera.get())
+    valoare=valoare.upper()
     litera.delete(0, END)
     if nr_imagini!=7:
         if cu==cuvant_nou:
             ok=1
-            fereastra_4()
+            fereastra3.destroy()
+            if ct==3:
+                fereastra0.destroy()
+                ct=2
         else:
             if valoare in litere_folosite:
                 nr_imagini = nr_imagini + 1
@@ -58,7 +56,7 @@ def gaseste(event):
                     if cu[i] == valoare:
                         cuvant_nou=cuvant_nou[:i]+valoare+cuvant_nou[i+1:]
                 litere_folosite.append(valoare)
-                cv = Label(text=cuvant_nou, font="Jokerman 20", bg="lightblue", width=20)
+                cv = Label(fereastra3,text=cuvant_nou, font="Jokerman 20", bg="lightblue", width=20)
                 cv.place(x=350, y=200)
             else:
                 nr_imagini = nr_imagini + 1
@@ -70,7 +68,10 @@ def gaseste(event):
             lb.place(x=170, y=45)
     if nr_imagini==7:
         ok=0
-        fereastra_4()
+        fereastra3.destroy()
+        if ct==3:
+            fereastra0.destroy()
+            ct=2
 def introduce_litera():
     global litera
     litera = Entry(fereastra3, width=15)
@@ -80,7 +81,7 @@ def fereastra_3():
     global fereastra2
     global fereastra3
     fereastra2.destroy()
-    fereastra3 = Tk()
+    fereastra3 = Toplevel()
     fereastra3.title("Hangman Game")
     fereastra3.configure(bg="lightblue")
     fereastra3.geometry("700x500")
@@ -89,39 +90,55 @@ def fereastra_3():
     global litere_folosite
     global cuv
     global cu
+    global cuvinte_folosite
     global cuvant_nou
     litere_folosite=[]
     cu=''
     if x == "1":
-        lb = Label(text="Ati ales categoria Tari:", font="Jokerman 15", bg="lightblue")
+        lb = Label(fereastra3,text="Ati ales categoria Tari:", font="Jokerman 15", bg="lightblue")
         lb.pack(anchor=NW)
         cuv = random.choice(Tari)
+        while cuv in cuvinte_folosite:
+            cuv = random.choice(Tari)
+        cuvinte_folosite.append(cuv)
         cuv = cuv.upper()
     elif x == "2":
-        lb = Label(text="Ati ales categoria Orase:", font="Jokerman 15", bg="lightblue")
+        lb = Label(fereastra3,text="Ati ales categoria Orase:", font="Jokerman 15", bg="lightblue")
         lb.pack(anchor=NW)
         cuv = random.choice(Orase)
+        while cuv in cuvinte_folosite:
+            cuv = random.choice(Orase)
+        cuvinte_folosite.append(cuv)
         cuv = cuv.upper()
     elif x == "3":
-        lb = Label(text="Ati ales categoria Animale:", font="Jokerman 15", bg="lightblue")
+        lb = Label(fereastra3,text="Ati ales categoria Animale:", font="Jokerman 15", bg="lightblue")
         lb.pack(anchor=NW)
         cuv = random.choice(Animale)
+        while cuv in cuvinte_folosite:
+            cuv = random.choice(Animale)
+        cuvinte_folosite.append(cuv)
         cuv = cuv.upper()
     elif x == "4":
-        lb = Label(text="Ati ales categoria Random:", font="Jokerman 15", bg="lightblue")
+        lb = Label(fereastra3,text="Ati ales categoria Random:", font="Jokerman 15", bg="lightblue")
         lb.pack(anchor=NW)
         cuv = random.choice(Random)
+        while cuv in cuvinte_folosite:
+            cuv = random.choice(Random)
+        cuvinte_folosite.append(cuv)
         cuv = cuv.upper()
     else:
-        lb = Label(text="Vi se va atribui un cuvant Random:", font="Jokerman 15", bg="lightblue")
+        lb = Label(fereastra3,text="Vi se va atribui un cuvant Random:", font="Jokerman 15", bg="lightblue")
         lb.pack(anchor=NW)
         cuv = random.choice(Random)
+        while cuv in cuvinte_folosite:
+            cuv = random.choice(Random)
+        cuvinte_folosite.append(cuv)
         cuv = cuv.upper()
     cuvant_nou = "_ " * len(cuv)
-    cuvant = Label(text=cuvant_nou, font="Jokerman 20", bg="lightblue", width=20)
+    cuvant = Label(fereastra3,text=cuvant_nou, font="Jokerman 20", bg="lightblue", width=20)
     cuvant.place(x=350, y=200)
-    lb1 = Label(text="Introduceti o litera de tipar:", font="Jokerman 15", bg="lightblue")
-    lb1.place(x=212, y=390)
+    lb1 = Label(fereastra3,text="Introduceti o litera:", font="Jokerman 15", bg="lightblue")
+    lb1.place(x=290, y=390)
     for i in range(len(cuv)):
         cu=cu+cuv[i]+" "
     display_img(Imagini[0])
@@ -140,7 +157,7 @@ def fereastra_2():
     global fereastra2
     global casuta_var
     fereastra.destroy()
-    fereastra2 = Tk()
+    fereastra2 = Toplevel()
     fereastra2.title("Hangman Game")
     fereastra2.configure(bg="lightblue")
     fereastra2.geometry("700x500")
@@ -149,10 +166,13 @@ def fereastra_2():
     casuta_var = Entry(fereastra2, width=15)
     casuta_var.place(x=290, y=375)
     casuta_var.bind("<Return>",eval)
-    fereastra2.mainloop()
 def fereastra1():
     global fereastra
-    fereastra=Tk()
+    global fereastra0
+    global cuvinte_folosite
+    global ct
+    cuvinte_folosite = []
+    fereastra=Toplevel()
     fereastra.title("Hangman Game")
     fereastra.configure(bg="lightblue")
     fereastra.geometry("700x500")
@@ -162,12 +182,30 @@ def fereastra1():
     text.place(x=20,y=370)
     img=PhotoImage(file="poza7.png")
     imagine=Label(fereastra,image=img)
+    imagine.photo=img
     imagine.place(x=230,y=95)
     imagine.configure(bg="lightblue")
+    if ct==3:
+        fereastra0.withdraw()
     b=Button(fereastra,text="Start",font="Jokerman 25",width=6,height=1,command=fereastra_2)
     b.place(x=270,y=350)
-    b.configure(bg="lightblue",anchor=CENTER,activebackground="gray")
-    fereastra.mainloop()
-fereastra1()
+    b.configure(bg="lightblue",activebackground="gray")
+def fereastra_0():
+    global ct
+    global fereastra0
+    ct=3
+    fereastra0 = Tk()
+    fereastra0.title("Hangman Game")
+    fereastra0.configure(bg="lightblue")
+    fereastra0.geometry("700x500")
+    b1= Button(fereastra0, text="Accesati jocul!", font="Jokerman 25", width=18,  command=fereastra1)
+    b1.place(x=150,y=200)
+    b1.configure(bg="lightblue", activebackground="gray")
+    fereastra0.mainloop()
+
+fereastra_0()
+
+
+
 
 
